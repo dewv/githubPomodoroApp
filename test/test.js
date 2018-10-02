4498b991f7a89db713ff5d71bc2eeae8d9b9f805
Steve Mattingly <mattinglys@dewv.edu>
2018-10-02 22:09:35 +0000

    Set up CI

---
+++ test/test.js
@@ -0,0 +1,13 @@
+"use strict";
+require("should");
+require("../app");
+/*
+ * This test will always pass. It is a placeholder to allow
+ * activation of status checks with Travis CI. Remove it
+ * after the status checks are in place.
+ */
+describe("This app", function() {
+    it("should pass all tests", function() {
+        (true).should.be.true();
+    });
+});
